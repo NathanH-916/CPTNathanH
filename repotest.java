b661b2fed8413813746a46fd0c6a1260ff5ee1da
NathanH-916 <nathan.han27@ycdsbk12.ca>
2025-05-23 10:51:28 -0400

    Create repotest.java

---
+++ repotest.java
@@ -0,0 +1,11 @@
+import arc.*;
+
+public class repotest{
+	public static void main (String[] args){
+		Console con = new Console();
+		con.println("Hello World");
+		con.println("TGIF");
+		con.println("CPT Time!");
+		
+	}
+}
