0e0e0fa47c866f67f3430866a71014ef054dba63
NathanH-916 <nathan.han27@ycdsbk12.ca>
2025-05-30 11:04:13 -0400

    Allowed for user input to select option
    Created main menu

---
+++ Cpt.java
@@ -0,0 +1,21 @@
+import arc.*;
+
+public class Cpt {
+
+    public static void main(String[] args) {
+        Console con = new Console();
+
+        int intChoice = 0;
+
+        while (intChoice != 4) {
+            con.println("===== Word Guessing Game =====");
+            con.println("1. Play Game");
+            con.println("2. View Leaderboard");
+            con.println("3. Add Quiz");
+            con.println("4. Quit");
+            con.println("Choose an option: ");
+            intChoice = con.readInt();
+         }
+           
+      }
+}
--- repotest.java
+++ repotest.java
@@ -2,12 +2,61 @@ import arc.*;
 
 public class repotest{
 	public static void main (String[] args){
-		Console con = new Console();
-		con.println("Hello World");
-		con.println("TGIF");
-		con.println("CPT Time!");
-		con.println("My name is nathan");
-		con.println("I like grapes");
-		
+			String strMovies[][];
+			
+			strMovies 
+			
+			
+			
+			strMove[1][0]
+			
+			
+			strMovie[3][1] = "69";
+			strMovie[3][2] = "93";
+
+			strMovie[4][0] = "Brutalist";
+			strMovie[4][1] = "93";
+			strMovie[4][2] = "80";
+
+			// print out array
+			int intCount;
+			con.println("Before sorting");
+			for(intCount = 0; intCount < 5; intCount++) {
+				con.println(strMovie[intCount][0] + " - " + strMovie[intCount][1] + " - " + strMovie[intCount][2]);
+			}
+
+			int intCount2;
+			String strNameTemp;
+			String strCriticsTemp;
+			String strPeopleTemp;
+
+			for(intCount = 0; intCount < 5-1; intCount++) {
+				if(Integer.parseInt(strMovie[intCount][1]) > Integer.parseInt(strMovie[intCount+1][1])){
+					
+					
+					strNameTemp = strMovie[intCount][0];
+					strMovie[intCount][0] = strMovie[intCount+1][0]
+					strMovie[intCount+1][0] = strNameTemp;
+					
+					strCriticsTemp = strMovie[intCount[1];
+					strMovie[intCount[1] = strMovie[intCount+1][1];
+					strMovie[intCount+1][1] = strCriticsTemp;
+					
+					streopleTemp = strMovie[intCount
+    )
+    }
+
+    con.println("\n\nAfter sorting");
+    for(intCount = 0; intCount < 5; intCount++) {
+    con.println(strMovie[intCount][0] + " - " + strMovie[intCount][1] + " - " + strMovie[intCount][2]);
+    }
+}
+
+//Users/acadams/Desktop/Coup Sci lib/src.jar:
+
+			
+			
+			
+			
 	}
 }
