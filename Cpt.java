7f4355ad390dda8aefcd6a2cba678a04aab59ae2
NathanH-916 <nathan.han27@ycdsbk12.ca>
2025-06-11 11:01:53 -0400

    Update Cpt.java
    Attempted bubble sort and need to fix parsing errors.

--- Cpt.java
+++ Cpt.java
@@ -1,88 +1,136 @@
 import arc.*;
 
-public class Cpt{
-	public static void main (String[] args){
+public class Cpt {
+	public static void main(String[] args) {
 		Console con = new Console();
 
-      
-        String themeName;        
-         
-        String word;           
-        String shownWord = "";  
-        int wordLength;           
-        int points;               
-        boolean isComplete = false; 
-        String userInput;        
-        char guessedLetter;       
-        String newShownWord;      
-        boolean found;            // True if guessed letter is in the word
-        int letter;               // Loop variable for going through letters
-        char realLetter;          // The actual letter in the original word
-        char currentLetter;       // The character currently shown (dash or guessed letter)
-        // ============================================
+		// Variables
+		String[] strThemes = new String[5];
+		String[] strWords = new String[5];
+		String strHidden = "";
+		String strGuess;
+		String strAnswer;
+		String strUser;
+		int intTries;
+		int intPoints;
+		int intCount;
+		int intPos;
+		char chrLetter;
+		boolean blnCorrect;
 
-        // Ask for theme file (e.g., food.txt)
-        con.print("Enter theme name (no .txt): ");
-        themeName = con.readLine();
+		// Load theme from file
+		TextInputFile themeFile = new TextInputFile("themes.txt");
+		intCount = 0;
+		while (themeFile.eof() == false && intCount < 5) {
+			strThemes[intCount] = themeFile.readLine();
+			strWords[intCount] = themeFile.readLine();
+			intCount++;
+		}
+		themeFile.close();
 
-   
+		// Ask for user name
+		con.print("Enter your name: ");
+		strUser = con.readLine();
 
-        wordLength = word.length();
-        points = wordLength;
+		boolean blnRunning = true;
+		while (blnRunning) {
+			con.println("\nMain Menu:");
+			con.println("1. Start Game");
+			con.println("2. View Leaderboard");
+			con.println("3. Exit");
+			con.print("Enter your choice: ");
+			int intMenuChoice = con.readInt();
 
-        // Fill shownWord with dashes
-        for (letter = 0; letter < wordLength; letter++) {
-            shownWord += "-";
-        }
+			if (intMenuChoice == 1) {
+				// Pick theme
+				con.println("Choose a theme:");
+				for (int intThemeIndex = 0; intThemeIndex < strThemes.length; intThemeIndex++) {
+					if (strThemes[intThemeIndex] != null) {
+						con.println((intThemeIndex + 1) + ". " + strThemes[intThemeIndex]);
+					}
+				}
+				int intChoice = con.readInt();
+				strAnswer = strWords[intChoice - 1];
 
-        while (points > 0 && !isComplete) {
-            con.println("Word: " + shownWord);
-            con.println("Points left: " + points);
-            con.print("Guess a letter: ");
-            userInput = con.readLine();
+				// Initialize hidden word
+				strHidden = "";
+				for (int intCharIndex = 0; intCharIndex < strAnswer.length(); intCharIndex++) {
+					strHidden = strHidden + "_";
+				}
 
-            if (userInput.length() != 1) {
-                con.println("Enter just one letter!");
-                continue;
-            }
+				intTries = 6;
+				intPoints = 0;
 
-            guessedLetter = userInput.charAt(0);
-            newShownWord = "";
-            found = false;
+				while (intTries > 0 && strHidden.indexOf("_") != -1) {
+					con.println("Word: " + strHidden);
+					con.print("Guess a letter: ");
+					strGuess = con.readLine();
+					chrLetter = strGuess.charAt(0);
 
-            // Go through the word letter by letter
-            for (letter = 0; letter < wordLength; letter++) {
-                realLetter = word.charAt(letter);
-                currentLetter = shownWord.charAt(letter);
+					blnCorrect = false;
+					for (int intCharIndex = 0; intCharIndex < strAnswer.length(); intCharIndex++) {
+						if (strAnswer.substring(intCharIndex, intCharIndex + 1).equalsIgnoreCase(strGuess)) {
+							strHidden = strHidden.substring(0, intCharIndex) + strAnswer.substring(intCharIndex, intCharIndex + 1) + strHidden.substring(intCharIndex + 1);
+							blnCorrect = true;
+						}
+					}
 
-                if (currentLetter != '-') {
-                    newShownWord += currentLetter; // Keep already guessed letters
-                } else if (realLetter == guessedLetter) {
-                    newShownWord += guessedLetter; // Correct guess
-                    found = true;
-                } else {
-                    newShownWord += "-"; // Still hidden
-                }
-            }
+					if (blnCorrect) {
+						intPoints = intPoints + 10;
+						con.println("Correct!");
+					} else {
+						intTries = intTries - 1;
+						con.println("Wrong! Tries left: " + intTries);
+					}
+				}
 
-            shownWord = newShownWord;
+				if (strHidden.equalsIgnoreCase(strAnswer)) {
+					con.println("You win! The word was: " + strAnswer);
+				} else {
+					con.println("Game over! The word was: " + strAnswer);
+				}
 
-            if (!found) {
-                points--;
-            }
+				// Leaderboard update
+				TextInputFile leaderIn = new TextInputFile("leaderboard.txt");
+				String[] strNames = new String[10];
+				int[] intScores = new int[10];
+				intCount = 0;
+				while (leaderIn.eof() == false && intCount < 10) {
+					strNames[intCount] = leaderIn.readLine();
+					intScores[intCount] = leaderIn.readInt();
+					intCount++;
+				}
+				leaderIn.close();
 
-            // Check if the word is fully guessed
-            if (shownWord.equals(word)) {
-                isComplete = true;
-            }
-        }
+				strNames[intCount] = strUser;
+				intScores[intCount] = intPoints;
+				intCount++;
 
-        if (isComplete) {
-            con.println("🎉 You guessed it! The word was: " + word);
-        } else {
-            con.println("❌ Out of points! The word was: " + word);
-        }
+				// Bubble sort
+				for (int intOuter = 0; intOuter < intCount - 1; intOuter++) {
+					for (int intInner = 0; intInner < intCount - 1 - intOuter; intInner++) {
+						if (intScores[intInner] < intScores[intInner + 1]) {
+							int intTemp = intScores[intInner];
+							intScores[intInner] = intScores[intInner + 1];
+							intScores[intInner + 1] = intTemp;
+							String strTemp = strNames[intInner];
+							strNames[intInner] = strNames[intInner + 1];
+							strNames[intInner + 1] = strTemp;
+						}
+					}
+				}
 
-        con.println("Game over.");
-    }
+				TextOutputFile leaderOut = new TextOutputFile("leaderboard.txt");
+				for (int intLeaderIndex = 0; intLeaderIndex < 10 && strNames[intLeaderIndex] != null; intLeaderIndex++) {
+					leaderOut.println(strNames[intLeaderIndex]);
+					leaderOut.println(intScores[intLeaderIndex]);
+				}
+				leaderOut.close();
+
+			} else if (intMenuChoice == 2) 
+				// Display leaderboard
+	
+
+
+
 }
